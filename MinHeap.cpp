b700a6e403ec1d3c6bb0e8ae583947e9486debc9
Vraj Shah <vshah9270@floridapoly.edu>
2026-04-11 23:07:49 +0000

    updtaed STD::

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -18,22 +18,22 @@ void MinHeap<T>::print() const {
     int new_level = 1;
 
     for(const T& i : data) {
-        std::cout << i << ' ';
+        cout << i << ' ';
         cur_level++;
         if (cur_level == new_level) {
-            std::cout << "\n";
+            cout << "\n";
             new_level *= 2;
             cur_level = 0;
         }
     }
-    std::cout << "\n----------------------------\n";
+    cout << "\n----------------------------\n";
 }
 
 
 template<typename T>
 T MinHeap<T>::delete_min() {
     if (data.empty()) {
-        throw std::string("delete_min: Empty Heap\n");
+        throw string("delete_min: Empty Heap\n");
     }    
     T res = data[0];
     data[0] = data[data.size() - 1]; //set the root with the value of the last node
@@ -84,7 +84,7 @@ void MinHeap<T>::percolate_down(int i) {
 template<typename T>
 int MinHeap<T>::min_index(int i1, int i2) const {
     if (i1 >= data.size() || i2 >= data.size() || i1 < 0 || i2 < 0) {
-        throw std::string ("min_index: incorrect index");
+        throw string ("min_index: incorrect index");
     }
 
     return (data[i1] < data[i2] ? i1 : i2);
